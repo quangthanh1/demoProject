990288ac32385251b37a92e3c8c77930a9038923
quang.thanh <quang.thanh@eastagile.com>
2018-07-03 15:09:06 +0700

    add config circleCI slather pod

--- QuangDemoCircleCI/QuangDemoCircleCITests/QuangDemoCircleCITests.swift
+++ QuangDemoCircleCI/QuangDemoCircleCITests/QuangDemoCircleCITests.swift
@@ -10,20 +10,24 @@ import XCTest
 @testable import QuangDemoCircleCI
 
 class QuangDemoCircleCITests: XCTestCase {
-    
+    var vc: ViewController!
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        vc = ViewController()
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        vc = nil
         super.tearDown()
     }
     
-    func testExample() {
+    func testValidate() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertFalse(vc.validateData(email: ""))
+        XCTAssertTrue(vc.validateData(email: "abc@123.com"))
     }
     
     func testPerformanceExample() {
